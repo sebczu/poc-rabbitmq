7bc57c7dbca13dc37731bb528cd72f55f6bb0811
sebastian.czubala.dev <sebastian.czubala.dev@gmail.com>
2020-10-25 13:07:28 +0100

    exchange header

---
+++ service/application/src/main/java/com/sebczu/poc/rabbitmq/ExchangeHeaderConfiguration.java
@@ -0,0 +1,45 @@
+package com.sebczu.poc.rabbitmq;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ExchangeHeaderConfiguration {
+
+  public static final String QUEUE_HEADER_USERS_NAME = "queue-header-users";
+  public static final String QUEUE_HEADER_USER_100_NAME = "queue-header-user-100";
+  public static final String QUEUE_HEADER_USER_200_NAME = "queue-header-user-200";
+
+  private static final String EXCHANGE_HEADER_NAME = "exchange-header";
+
+  @Bean
+  public Queue queueHeaderUsers() {
+    return new Queue(QUEUE_HEADER_USERS_NAME, false);
+  }
+
+  @Bean
+  public Queue queueHeaderUser100() {
+    return new Queue(QUEUE_HEADER_USER_100_NAME, false);
+  }
+
+  @Bean
+  public Queue queueHeaderUser200() {
+    return new Queue(QUEUE_HEADER_USER_200_NAME, false);
+  }
+
+  @Bean
+  public HeadersExchange exchangeHeader() {
+    return new HeadersExchange(EXCHANGE_HEADER_NAME);
+  }
+
+  @Bean
+  public Declarables headerBinding() {
+    return new Declarables(
+//        BindingBuilder.bind(queueHeaderUsers()).to(exchangeHeader()).where("user").matches("*"),
+        BindingBuilder.bind(queueHeaderUser100()).to(exchangeHeader()).where("user").matches("100"),
+        BindingBuilder.bind(queueHeaderUser200()).to(exchangeHeader()).where("user").matches("200")
+    );
+  }
+
+}
---
+++ service/application/src/main/java/com/sebczu/poc/rabbitmq/controller/ExchangeHeaderController.java
@@ -0,0 +1,65 @@
+package com.sebczu.poc.rabbitmq.controller;
+
+import com.sebczu.poc.rabbitmq.ExchangeHeaderConfiguration;
+import com.sebczu.poc.rabbitmq.ExchangeTopicConfiguration;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/exchange/header")
+@RequiredArgsConstructor
+public class ExchangeHeaderController {
+
+  private final RabbitTemplate rabbitTemplate;
+  private final HeadersExchange headerExchange;
+  private final Queue queueHeaderUsers;
+  private final Queue queueHeaderUser100;
+  private final Queue queueHeaderUser200;
+
+  @PostMapping("/user100")
+  public void sendToExchangeHeaderUser100(@RequestParam(value = "message") String text) {
+    MessageProperties messageProperties = new MessageProperties();
+    messageProperties.setHeader("user", "100");
+    MessageConverter messageConverter = new SimpleMessageConverter();
+    Message message = messageConverter.toMessage(text, messageProperties);
+
+    rabbitTemplate.send(headerExchange.getName(), "", message);
+    log.info("message: {} send into exchange: {} with header: {}", text, headerExchange.getName(), "user=100");
+  }
+
+  @PostMapping("/user200")
+  public void sendToExchangeHeaderUser200(@RequestParam(value = "message") String text) {
+    MessageProperties messageProperties = new MessageProperties();
+    messageProperties.setHeader("user", "200");
+    MessageConverter messageConverter = new SimpleMessageConverter();
+    Message message = messageConverter.toMessage(text, messageProperties);
+
+    rabbitTemplate.send(headerExchange.getName(), "", message);
+    log.info("message: {} send into exchange: {} with header: {}", text, headerExchange.getName(), "user=200");
+  }
+
+  @RabbitListener(queues = ExchangeHeaderConfiguration.QUEUE_HEADER_USERS_NAME)
+  public void getFromQueueHeaderUsers(Message message) {
+    log.info("message: {} receive from queue: {}", new String(message.getBody()), queueHeaderUsers.getName());
+  }
+
+  @RabbitListener(queues = ExchangeHeaderConfiguration.QUEUE_HEADER_USER_100_NAME)
+  public void getFromQueueHeaderUser100(Message message) {
+    log.info("message: {} receive from queue: {}", new String(message.getBody()), queueHeaderUser100.getName());
+  }
+
+  @RabbitListener(queues = ExchangeHeaderConfiguration.QUEUE_HEADER_USER_200_NAME)
+  public void getFromQueueHeaderUser200(Message message) {
+    log.info("message: {} receive from queue: {}", new String(message.getBody()), queueHeaderUser200.getName());
+  }
+}
